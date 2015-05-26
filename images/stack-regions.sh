81df31ba1c0d4e9b96b0c8d808ddc06376cd1d67
marcolino <marcosolari@gmail.com>
2015-05-26 20:03:57 +0200

    cleaned up

---
+++ images/stack-regions.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+convert \
+africa.png \
+asia.png \
+caribbean.png \
+central-america.png \
+eastern-europe.png \
+europe.png \
+middle-east.png \
+north-america.png \
+oceania.png \
+south-america.png \
+\
+-append \
+regions32.png
